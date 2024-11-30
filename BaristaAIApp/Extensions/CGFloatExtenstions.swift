5c493492cebdc234aff3268e04e3d2eb87273aed
Kimsuncheol <cheonjae6@naver.com>
2024-11-30 12:15:42 +0900

    I added function showing an image of each drink

---
+++ BaristaAIApp/Extensions/CGFloatExtenstions.swift
@@ -0,0 +1,8 @@
+//
+//  CGFloatExtenstions.swift
+//  BaristaAIApp
+//
+//  Created by 김선철 on 11/29/24.
+//
+
+import Foundation
---
+++ BaristaAIApp/Extensions/StringExtensions.swift
@@ -0,0 +1,8 @@
+//
+//  StringExtensions.swift
+//  BaristaAIApp
+//
+//  Created by 김선철 on 11/29/24.
+//
+
+import Foundation
